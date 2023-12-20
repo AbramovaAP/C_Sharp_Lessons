11ca3fd296828dcad6b20d5692c1530e68f84c38
Anna <shtefanova.1996@mail.ru>
2023-12-21 01:29:55 +0300

    Add some variables in Lesson_1: Task2

---
+++ Lesson_1/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+
+long l1 = 6;
+long sum2 = i1 + l1;
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+
+int product = i1 * i2;
+double product2 = i1 * d1;
+
+int quotient = i1 / i2;
+d1=6;
+double quotient2 = i1 / d1;
+
+Console.WriteLine(sum);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
+Console.WriteLine(product);
+Console.WriteLine(product2);
+Console.WriteLine(quotient);
+Console.WriteLine(quotient2);
