fa3a50ca01b358862a34a59ddd777b568a1dcc1b
Anna <shtefanova.1996@mail.ru>
2024-01-09 18:00:16 +0300

    Add Lesson_2: Task4 - FOR, FOREACH

--- Lesson_2/Task4/Program.cs
+++ Lesson_2/Task4/Program.cs
@@ -4,10 +4,25 @@ int[] array = { 58, 2, 3, 4, 5, 168, 7, 8, 9, 13 };
 int i = 0;
 int max  = array[0];
 
-while(i < N) {
-    if(array[i] > max) {
-        max = array[i];
-    }
-    i = i + 1;
+                  //WHILE
+// while(i < N) {
+//     if(array[i] > max) {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+                   //FOR
+// for (int i = 0; i < N; i++) {
+//     if(array[i] > max) {
+//         max = array[i];}
+// }
+// Console.WriteLine(max);
+
+                   //FOREACH
+foreach (int element in array) {
+    if(element > max) {
+        max = element;}
 }
 Console.WriteLine(max);
