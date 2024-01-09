dd8a564b5bc5f8c36b1b83a8db2d5ecf923404cc
Anna <shtefanova.1996@mail.ru>
2024-01-09 17:24:02 +0300

    Add Lesson_2: Task4

---
+++ Lesson_2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = 10;
+
+int[] array = { 58, 2, 3, 4, 5, 168, 7, 8, 9, 13 };
+int i = 0;
+int max  = array[0];
+
+while(i < N) {
+    if(array[i] > max) {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
