53d02869375fc51dceeb3296096e031b25785eca
Anna <shtefanova.1996@mail.ru>
2024-01-13 22:44:50 +0300

    Add Lesson_3: Task2

---
+++ Lesson_3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Создаем функцию
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i = i + 1;
+    }
+}
+//Запускаем функцию с необходимым аргументом
+PrintSquares (5);
+PrintSquares (10);
+PrintSquares (15);
