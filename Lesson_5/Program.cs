1a99bc32e462582273b6319877dec3817ef1ea4a
Anna <shtefanova.1996@mail.ru>
2024-01-29 12:38:20 +0300

    Add Lesson_5 & solved tasks

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,42 @@
+﻿// //Задача 1
+// // Вычислить факториал из натурального числа N!
+
+// int Fact(int n)
+// {
+//     int result = 1;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         result = result * i;
+//     }
+//     Console.WriteLine(result);
+//     return result;
+// }
+// int n = 5;
+// Fact (n);
+
+//==========================================================================
+
+//Работа с рекурсией
+// Вычислить факториал из натурального числа N!
+
+int Fact(int n)
+{
+    if(n == 1)
+    {
+        Console.WriteLine($"Stop requrson: n = {n}");
+        return n;
+    }
+    else {Console.WriteLine(n);}
+
+    int result =  n * Fact(n-1);
+
+    for (int i = 2; i <= n; i++)
+    {
+        Console.WriteLine($"Возврат: n = {i}, fact = {Fact(i-1)}");
+    }
+
+    return result ;
+    
+}
+int n = 5;
+Console.WriteLine(Fact (n));
